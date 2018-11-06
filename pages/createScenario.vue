bfd01a65f183d3eec4b5de0d2e821777b5f5730a
“Minoru” <miyarappo@gmail.com>
2018-11-06 16:06:54 +0900

    wip

---
+++ pages/createScenario.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-container>
+    <el-header>新規のシナリオ</el-header>
+    <el-container>
+      <el-aside width="400px">
+        <el-select
+          v-model="value"
+          placeholder="サービスを選択"
+          @change="changeValue(value)">
+          <el-option
+            v-for="(item, index) in serviceOptions"
+            :key="index"
+            :label="item.label"
+            :value="item.value" />
+        </el-select>
+      </el-aside>
+      <el-main>フローに追加する行動を選択してください</el-main>
+    </el-container>
+  </el-container> 
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      serviceOptions: [{
+          value: 'b_value',
+          label: 'ブラウザ'
+        }, {
+          value: 'g_value',
+          label: 'Gmail'
+        }, {
+          value: 's_value',
+          label: 'スプレッドシート'
+        }],
+      value: ''
+    }
+  },
+  created() {
+    console.log(this)
+  },
+  methods: {
+    changeValue(value) {
+      console.log(value)
+    }
+  }
+}
+</script>
+
+<style>
+  .el-container {
+    color: #333;
+  }
+  .el-header {
+    background-color: #B3C0D1;
+    height: 60px;
+    line-height: 60px;
+  }
+  
+  .el-main {
+    background-color: #F2F2F2;
+    height: calc(100vh - 60px);
+  }
+  .box-card {
+    width: 228px;
+    display: inline-block;
+    margin: 10px 20px 10px 0;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,60 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        nuxt-elementui-sample
-      </h1>
-      <h2 class="subtitle">
-        My wondrous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <el-container>
+    <el-header>
+      <el-button
+        type="primary"
+        icon="el-icon-edit"
+        @click="goCreateScenario">シナリオを作成
+      </el-button>
+    </el-header>
+    <el-main>
+      <el-card
+        v-for="(template, index) in templates"
+        :key="index"
+        class="box-card">
+        <div>{{ template }}</div>
+      </el-card>
+    </el-main>
+  </el-container> 
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      templates: [
+        "人事労務系のテンプレート",
+        "顧客管理系のテンプレート",
+        "営業支援系のテンプレート",
+        "WEBサイト分析系のテンプレート"
+      ]
+    }
+  },
+  methods: {
+    goCreateScenario() {
+      this.$router.push({ path: 'createScenario' })
+    }
   }
 }
 </script>
 
 <style>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .el-container {
+    color: #333;
+  }
+  .el-header {
+    background-color: #B3C0D1;
+    height: 60px;
+    line-height: 60px;
+  }
+  
+  .el-main {
+    background-color: #F2F2F2;
+    height: calc(100vh - 60px);
+  }
+  .box-card {
+    width: 228px;
+    display: inline-block;
+    margin: 10px 20px 10px 0;
+  }
 </style>
